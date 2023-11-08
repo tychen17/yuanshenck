39ddf3bdfc147a47bf692b5639daad370f0e20fb
tengyuechen <102759255+tychen17@users.noreply.github.com>
2023-11-08 12:12:11 -0500

    Update yuanshenchouka.py

--- yuanshenchouka.py
+++ yuanshenchouka.py
@@ -14,44 +14,51 @@ RECHARGE_TIERS = {
 # 原石和结晶兑换抽数的比例
 PRIMO_TO_PULL = 160  # 160原石兑换一次抽卡
 
-# 大保底的抽数
-MAX_PITY = 160
-
-# 抽卡的价格（人民币），每次抽卡按照最小充值档次计算
-PRICE_PER_PULL = min(RECHARGE_TIERS.keys())
-
-# 计算所需的原石数量
-def calculate_primos_needed(desired_constellations, desired_pulls, existing_pulls, padded_pulls):
-    if desired_constellations > 0:
-        # 如果设定了需要的命座数
-        total_pulls_needed = desired_constellations * MAX_PITY
-    elif desired_pulls > 0:
-        # 如果设定了期望的抽数
+# 计算可抽取的命座数
+def calculate_constellations(primo, crystal, padded_pulls, existing_pulls, desired_constellations, desired_pulls):
+    if desired_constellations:
+        total_pulls_needed = desired_constellations * padded_pulls
+    elif desired_pulls:
         total_pulls_needed = desired_pulls
     else:
         return None, "请输入需要的命座数或期望抽数。"
     
-    pulls_remaining_after_existing = max(total_pulls_needed - existing_pulls - padded_pulls, 0)
-    primos_needed = pulls_remaining_after_existing * PRIMO_TO_PULL
-    return primos_needed, ""
+    total_pulls_available = primo // PRIMO_TO_PULL + crystal // PRIMO_TO_PULL + existing_pulls
+    constellations_obtained = min(total_pulls_available // padded_pulls, desired_constellations)
+    remaining_pulls = total_pulls_available - constellations_obtained * padded_pulls
+    return constellations_obtained, remaining_pulls
 
-# 计算充值方案
-def find_best_recharge_plan(primos_needed):
-    sorted_tiers = sorted(RECHARGE_TIERS.items(), key=lambda item: item[1], reverse=True)
+# 计算还需充值多少钱及充值档次选择
+def calculate_recharge(primo, crystal, padded_pulls, existing_pulls, desired_constellations, desired_pulls):
+    constellations_obtained, remaining_pulls = calculate_constellations(primo, crystal, padded_pulls, existing_pulls, desired_constellations, desired_pulls)
+    if desired_constellations:
+        total_constellations_needed = desired_constellations - constellations_obtained
+        pulls_needed = total_constellations_needed * padded_pulls
+    else:
+        pulls_needed = desired_pulls - remaining_pulls
+    
+    primo_needed = pulls_needed * PRIMO_TO_PULL - primo - (crystal * PRIMO_TO_PULL)
+    
+    money_needed = 0
     recharge_plan = []
-    for primo, cost in sorted_tiers:
-        while primos_needed >= primo:
-            primos_needed -= primo
+
+    # Sort by cost effectiveness (amount of primo per unit of currency)
+    sorted_tiers = sorted(RECHARGE_TIERS.items(), key=lambda item: item[1] / item[0], reverse=True)
+    for primo_amount, cost in sorted_tiers:
+        while primo_needed >= primo_amount:
+            primo_needed -= primo_amount
+            money_needed += cost
             recharge_plan.append(cost)
-    if primos_needed > 0:
-        # 如果还有剩余，就选择下一个最小的档次
-        for primo, cost in sorted(RECHARGE_TIERS.items(), key=lambda item: item[0]):
-            if primos_needed > 0:
-                primos_needed -= primo
+    
+    # Add the smallest necessary tier if there's still a primo deficit
+    if primo_needed > 0:
+        for primo_amount, cost in sorted(RECHARGE_TIERS.items(), key=lambda item: item[0]):
+            if primo_needed <= primo_amount:
+                money_needed += cost
                 recharge_plan.append(cost)
                 break
-    total_cost = sum(recharge_plan)
-    return recharge_plan, total_cost
+
+    return money_needed, recharge_plan, remaining_pulls
 
 # 执行计算并显示结果
 def calculate():
@@ -63,26 +70,48 @@ def calculate():
         desired_constellations = int(desired_constellations_entry.get())
         desired_pulls = int(desired_pulls_entry.get())
 
-        # 计算所需原石
-        primos_needed, message = calculate_primos_needed(desired_constellations, desired_pulls, existing_pulls, padded_pulls)
-        if message:
-            messagebox.showerror("错误", message)
-            return
+        money_needed, recharge_plan, remaining_pulls = calculate_recharge(primo, crystal, padded_pulls, existing_pulls, desired_constellations, desired_pulls)
 
-        # 减去用户已有的原石和结晶
-        primos_needed -= (primo + crystal * PRIMO_TO_PULL)
-
-        # 计算充值方案
-        recharge_plan, total_cost = find_best_recharge_plan(primos_needed)
-
-        result_message = f"总计需要充值金额为：{total_cost}元\n" \
+        result_message = f"目前能抽出的命座数为：{desired_constellations}\n" \
+                         f"还需充值金额为：{money_needed}元\n" \
                          f"充值档次选择的最优解为：{recharge_plan}\n" \
-                         f"总计需要抽数为：{desired_pulls}\n" \
-                         f"总计需要命座数为：{desired_constellations}"
-
+                         f"还剩的抽取数为：{remaining_pulls}"
         messagebox.showinfo("抽卡规划结果", result_message)
-
     except ValueError:
         messagebox.showerror("错误", "请输入正确的数值！")
 
-# GUI布局和运行主循环的部分与您提供的相同
+# 创建主窗口
+root = tk.Tk()
+root.title("原神抽卡规划程序")
+
+# 创建输入框标签和输入框
+tk.Label(root, text="原石数量").grid(row=0, column=0)
+primo_entry = tk.Entry(root)
+primo_entry.grid(row=0, column=1)
+
+tk.Label(root, text="结晶数量").grid(row=1, column=0)
+crystal_entry = tk.Entry(root)
+crystal_entry.grid(row=1, column=1)
+
+tk.Label(root, text="垫的抽数（大保底）").grid(row=2, column=0)
+padded_pulls_entry = tk.Entry(root)
+padded_pulls_entry.grid(row=2, column=1)
+
+tk.Label(root, text="已有抽数").grid(row=3, column=0)
+existing_pulls_entry = tk.Entry(root)
+existing_pulls_entry.grid(row=3, column=1)
+
+tk.Label(root, text="需要命座数").grid(row=4, column=0)
+desired_constellations_entry = tk.Entry(root)
+desired_constellations_entry.grid(row=4, column=1)
+
+tk.Label(root, text="期望抽数").grid(row=5, column=0)
+desired_pulls_entry = tk.Entry(root)
+desired_pulls_entry.grid(row=5, column=1)
+
+# 创建计算按钮
+calculate_button = tk.Button(root, text="计算", command=calculate)
+calculate_button.grid(row=6, columnspan=2)
+
+# 运行主循环
+root.mainloop()
